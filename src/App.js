7140f4b65979249245e9d12907fa19bcb254cac1
zeckstone <aw.afriyie@gmail.com>
2021-08-03 14:09:22 -0400

    added page naviagtion via react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import HomePage from './pages/hompage/hompage.component';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop/hats' component={HatsPage}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
